5de816afd9469e4da24a006f9edef3c278aa5661
Alberto <albertus82@users.noreply.github.com>
2019-10-18 22:16:02 +0200

    Moved test into tests dir.

--- tests/test_jarver.py
+++ tests/test_jarver.py
@@ -1,17 +1,20 @@
+import os
 import unittest
 import zipfile
 
 import jarver
 
+TESTS_DIR = os.path.dirname(__file__)
+
 
 class TestJarver(unittest.TestCase):
 
     def test_analyze_classes(self):
-        with zipfile.ZipFile("dummy.jar") as file:
+        with zipfile.ZipFile(os.path.join(TESTS_DIR, "dummy.jar")) as file:
             self.assertEqual(jarver.analyze_classes(file), {(55, 0): ["DummyClass.class"]})
 
     def test_main(self):
-        self.assertIsNone(jarver.main(["dummy.jar"]))
+        self.assertIsNone(jarver.main([os.path.join(TESTS_DIR, "dummy.jar")]))
 
 
 if __name__ == '__main__':
