8ec8325777242b3740c46b12d24261752dd3067f
Alberto <albertus82@users.noreply.github.com>
2019-10-18 20:16:40 +0200

    Test.

---
+++ test_jarver.py
@@ -0,0 +1,18 @@
+import unittest
+import zipfile
+
+import jarver
+
+
+class TestJarver(unittest.TestCase):
+
+    def test_analyze_classes(self):
+        with zipfile.ZipFile("dummy.jar") as file:
+            self.assertEqual(jarver.analyze_classes(file), {(55, 0): ["DummyClass.class"]})
+            
+    def test_main(self):
+        self.assertIsNone(jarver.main(["dummy.jar"]))
+
+
+if __name__ == '__main__':
+    unittest.main()
