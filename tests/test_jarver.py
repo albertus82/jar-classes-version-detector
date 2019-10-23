45a540c337e2ba004690bba25ba78e58b18802f9
Alberto <albertus82@users.noreply.github.com>
2019-10-23 09:35:39 +0200

    Improved test.

--- tests/test_jarver.py
+++ tests/test_jarver.py
@@ -11,7 +11,7 @@ class TestJarver(unittest.TestCase):
 
     def test_analyze_classes(self):
         with zipfile.ZipFile(os.path.join(TESTS_DIR, "dummy.jar")) as file:
-            self.assertEqual(jarver.analyze_classes(file), {(55, 0): ["DummyClass.class"]})
+            self.assertEqual(jarver.analyze_classes(file), {(57, 0): ["DummyClass13.class"], (56, 0): ["DummyClass12.class"], (55, 0): ["DummyClass11.class"], (54, 0): ["DummyClass10.class"], (53, 0): ["DummyClass9.class"], (52, 0): ["DummyClass8.class"], (51, 0): ["DummyClass7.class"], (50, 0): ["DummyClass6.class"], (49, 0): ["DummyClass5.class"], (48, 0): ["DummyClass1_4.class"], (47, 0): ["DummyClass1_3.class"], (46, 0): ["DummyClass1_2.class"], (45, 3): ["DummyClass1_1.class"]})
 
     def test_main(self):
         self.assertIsNone(jarver.main([os.path.join(TESTS_DIR, "dummy.jar")]))
